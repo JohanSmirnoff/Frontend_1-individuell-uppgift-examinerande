1142134cb55479eb2a1f32aaea18e0bb88b3da87
JohanSmirnoff <smirnoff.johan@gmail.com>
2025-09-24 11:44:12 +0200

    progress

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
 const formBox = document.getElementById("post-form")
-const mainBox = document.querySelector(".main-container")
-const postForm = document.getElementById("post-form")
+const mainDiv = document.querySelector(".main-container")
+// const postForm = document.getElementById("post-form")
 const sendButton = document.getElementById("send-button")
 const resetButton = document.getElementById("reset-button")
+
+const blogPosts = []
+
+// En funktion för att skicka in userinput i en array, (trimma till små bokstäver) 
+function addUserInput(input) {
+    return {
+        id: crypto.randomUUID(),
+        author: input.get("user-name"),
+        title: input.get("user-title"),
+        message: input.get("user-message"),
+        timestamp: new Date().toLocaleTimeString()
+    }
+}
+
+function renderUserInput(blogPost) {
+    const blogDiv = document.createElement("div")
+    blogDiv.className = "blog-container"
+
+    const userName = document.createElement("p")
+    const userTitle = document.createElement("p")
+    const userMessage = document.createElement("p")
+    const timeStamp = document.createElement("p")
+
+    userName.textContent = blogPost.author
+    userTitle.textContent = blogPost.title
+    userMessage.textContent = blogPost.message
+    timeStamp.textContent = blogPost.timestamp
+    
+    blogDiv.append(userName, userTitle, userMessage, timeStamp)
+    mainDiv.appendChild(blogDiv)
+}
 
 
 formBox.addEventListener("submit", (e) => {
     e.preventDefault()
-    const formData = new FormData(formBox)
-    const userTitle = formData.get("user-title")
-    const userMessagae = formData.get("user-message")
-    const presentTime = new Date()
-    const postTime = presentTime.toLocaleTimeString()
-    const ppp = document.createElement("p")
-    ppp.textContent = postTime
-
+    const formInput = new FormData(formBox)
+    const author = formInput.get("user-name")
+    const title = formInput.get("user-title")
+    const message = formInput.get("user-message")
     
-    mainBox.appendChild(ppp)
-    console.log(ppp)
+    const createdBlogPost = addUserInput(formInput)
+    blogPosts.push(createdBlogPost)
+    renderUserInput(createdBlogPost)
     
-
-    
+    formBox.reset()
+    console.log(blogPosts)
 })
+
+
+
+
+
+
+
+
+
 
 // notes för imorgon
 
@@ -37,26 +74,3 @@ formBox.addEventListener("submit", (e) => {
 // Vid submit: skapa ett objekt → pusha in i arrayen → spara arrayen i localStorage → rendera posten.
 
 // Vid sidladdning: läs från localStorage → rendera alla.
-
-
-
-
-
-
-
-// function createPost() {
-//     const 
-// }
-
-
-// function createPost(userPost) {
-//     const postDiv = document.createElement("div")
-//     postDiv.id = "post-div"
-
-
-
-//     const message = document.createElement("p")
-//     message.id = "user-message"
-
-
-// }
